38e585f511e0950ccab28ea5623a1806ed04ac7e
null_mason <123850538+DiscoKnax6808@users.noreply.github.com>
2026-05-17 12:41:23 -0400

    switch syntax to support FETCH

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const fastify = require('fastify')({logger: true});
+const fastify = require('fastify')({ logger: true });
 const db = require('./db');
+const readline = require('node:readline');
 
+const version = "v0.01";
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-routefiles = [
-    "nameserver"
-]
+["nameserver"].forEach(file => fastify.register(require(`./routes/${file}`)));
 
-routefiles.forEach((filename) => {
-    fastify.register(require(`./routes/${filename}`));
-})
+console.log("Welcome to DrangoR! Made by null_mason/nomason");
 
-
-
-
-
-
-
-
-
-
-
-
-fastify.listen({port: 2059, host: "0.0.0.0"}, (err, address) => {
-    if (err) {
-        fastify.log.error(err);
-        process.exit(1);
+(async () => {
+    try {
+        const res = await fetch('https://githubusercontent.com');
+        if (!res.ok) throw new Error();
+        
+        const latest = (await res.text()).trim();
+        console.log(latest === version ? `Latest version: ${version}` : `Update available! Latest: ${latest}`);
+    } catch {
+        console.log("Unable to fetch latest version!");
     }
 
+    rl.question('Please Enter Your Choice Here: ', (choice) => {
+        console.log(`Selected: ${choice}`);
+    });
+})();
+
+fastify.listen({ port: 2059, host: "0.0.0.0" }, (err, address) => {
+    if (err) { fastify.log.error(err); process.exit(1); }
     console.log(`Server running at ${address}`);
-})
+});
