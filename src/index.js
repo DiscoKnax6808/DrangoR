9217b091480ac3d2e3892febd1f881807ade789f
null_mason <123850538+DiscoKnax6808@users.noreply.github.com>
2026-05-17 12:43:11 -0400

    Revert back to old syntax

--- src/index.js
+++ src/index.js
@@ -1,31 +1,62 @@
-const fastify = require('fastify')({ logger: true });
+const fastify = require('fastify')({logger: true});
 const db = require('./db');
 const readline = require('node:readline');
 
 const version = "v0.01";
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-["nameserver"].forEach(file => fastify.register(require(`./routes/${file}`)));
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+routefiles = [
+    "nameserver"
+]
+
+routefiles.forEach((filename) => {
+    fastify.register(require(`./routes/${filename}`));
+})
+
+
+async function getVersion()
+{
+    const versionresp = await fetch('https://raw.githubusercontent.com/DiscoKnax6808/DrangoRData/refs/heads/main/LatestVersion.txt');
+
+if (!Response.ok) {
+    console.log("Unable to fetch latest version!");
+} else {
+    const v = await versionresp.text();
+    if (v == version)
+    {
+        console.log(`You are on the latest version!: ${version}`);
+    } else {
+        console.log(`You aren't on the latest version! LatestVersion: ${v}`)
+    }
+}
+}
+
+
 
 console.log("Welcome to DrangoR! Made by null_mason/nomason");
 
-(async () => {
-    try {
-        const res = await fetch('https://githubusercontent.com');
-        if (!res.ok) throw new Error();
-        
-        const latest = (await res.text()).trim();
-        console.log(latest === version ? `Latest version: ${version}` : `Update available! Latest: ${latest}`);
-    } catch {
-        console.log("Unable to fetch latest version!");
+
+
+await getVersion();
+
+
+rl.question('Please Enter Your Choice Here: ')
+
+
+
+
+
+
+fastify.listen({port: 2059, host: "0.0.0.0"}, (err, address) => {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
     }
 
-    rl.question('Please Enter Your Choice Here: ', (choice) => {
-        console.log(`Selected: ${choice}`);
-    });
-})();
-
-fastify.listen({ port: 2059, host: "0.0.0.0" }, (err, address) => {
-    if (err) { fastify.log.error(err); process.exit(1); }
     console.log(`Server running at ${address}`);
-});
+})
